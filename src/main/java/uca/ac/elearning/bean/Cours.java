1d7b54a287e48d72fbd324b81afe850a9edef7a2
yassirab02 <yassirabourial86@gmail.com>
2024-02-19 16:28:29 +0100

    Cours,GroupeEtudiant

--- src/main/java/uca/ac/elearning/bean/Cours.java
+++ src/main/java/uca/ac/elearning/bean/Cours.java
@@ -9,6 +9,39 @@ public class Cours {
 
     private long id;
     private String libelle;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Parcours getParcours() {
+        return parcours;
+    }
+
+    public void setParcours(Parcours parcours) {
+        this.parcours = parcours;
+    }
+
     private String code;
     @ManyToOne
     private Parcours parcours;
