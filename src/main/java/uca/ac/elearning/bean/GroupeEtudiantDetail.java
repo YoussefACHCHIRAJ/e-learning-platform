f4abf55bf658d862847b28b05292ad0db9c9c167
Abderrahmanghazouani <152079577+Abderrahmanghazouani@users.noreply.github.com>
2024-02-19 16:10:49 +0100

    section grpEtudDet TypeMulti

--- src/main/java/uca/ac/elearning/bean/GroupeEtudiantDetail.java
+++ src/main/java/uca/ac/elearning/bean/GroupeEtudiantDetail.java
@@ -2,9 +2,17 @@ package uca.ac.elearning.bean;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class GroupeEtudiantDetail {
     @Id
     private long id;
+
+    private boolean active;
+
+    @OneToOne
+    private GroupeEtudiant groupeEtudiant;
+
 }
