7729bd4d04512f54ffd5510a92eda89c9881a10d
yassirab02 <yassirabourial86@gmail.com>
2024-02-19 16:10:23 +0100

    Cours,GroupeEtudient

--- src/main/java/uca/ac/elearning/bean/Cours.java
+++ src/main/java/uca/ac/elearning/bean/Cours.java
@@ -1,10 +1,15 @@
 package uca.ac.elearning.bean;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Cours {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private long id;
+    private String libelle;
+    private String code;
+    @ManyToOne
+    private Parcours parcours;
 }
--- src/main/java/uca/ac/elearning/bean/GroupeEtudiant.java
+++ src/main/java/uca/ac/elearning/bean/GroupeEtudiant.java
@@ -1,10 +1,20 @@
 package uca.ac.elearning.bean;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 public class GroupeEtudiant {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private long id;
+    private String libelle;
+    private LocalDate date;
+    @OneToOne
+    private GroupeEtudiantDetail groupeEtudiantDetail;
+    @ManyToOne
+    private Parcours parcours;
+
 }
