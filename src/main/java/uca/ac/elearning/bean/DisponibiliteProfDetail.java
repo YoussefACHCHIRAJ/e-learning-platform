63d3359e231ee07fed73c3b5ffd76f8acea4caa6
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2024-02-20 08:03:13 +0100

    create admin, disponibiliteProfDetail and category beans

--- src/main/java/uca/ac/elearning/bean/DisponibiliteProfDetail.java
+++ src/main/java/uca/ac/elearning/bean/DisponibiliteProfDetail.java
@@ -1,10 +1,56 @@
 package uca.ac.elearning.bean;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 public class DisponibiliteProfDetail {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    private LocalDate jour;
+    private LocalTime tranchHoraireDebut;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getJour() {
+        return jour;
+    }
+
+    public void setJour(LocalDate jour) {
+        this.jour = jour;
+    }
+
+    public LocalTime getTranchHoraireDebut() {
+        return tranchHoraireDebut;
+    }
+
+    public void setTranchHoraireDebut(LocalTime tranchHoraireDebut) {
+        this.tranchHoraireDebut = tranchHoraireDebut;
+    }
+
+    public LocalTime getTranchHoraireFin() {
+        return tranchHoraireFin;
+    }
+
+    public void setTranchHoraireFin(LocalTime tranchHoraireFin) {
+        this.tranchHoraireFin = tranchHoraireFin;
+    }
+
+    private LocalTime tranchHoraireFin;
+
+
+
+
+
 }
