0c7b808767514795739bd4a2ebd15980d6eb5c80
ROUSSI Hatim <115400374+ROVSSI@users.noreply.github.com>
2024-02-20 23:11:03 +0100

    add ParcoursDao and EtatParcoursDao

--- src/main/java/uca/ac/elearning/dao/EtatParcoursDao.java
+++ src/main/java/uca/ac/elearning/dao/EtatParcoursDao.java
@@ -1,8 +1,10 @@
 package uca.ac.elearning.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import uca.ac.elearning.bean.EtatParcours;
 
+@Repository
 public interface EtatParcoursDao extends JpaRepository<EtatParcours, Long> {
     EtatParcours findByCode(String code);
     EtatParcours findByLibelle(String libelle);
--- src/main/java/uca/ac/elearning/dao/ParcoursDao.java
+++ src/main/java/uca/ac/elearning/dao/ParcoursDao.java
@@ -1,8 +1,10 @@
 package uca.ac.elearning.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import uca.ac.elearning.bean.Parcours;
 
+@Repository
 public interface ParcoursDao extends JpaRepository<Parcours, Long> {
     Parcours findByLibelle(String Libelle);
     Parcours findByEtatParcoursCode(String code);
