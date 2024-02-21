872b073dbfe7f51db629a5454fb58d55be6e9767
ROUSSI Hatim <115400374+ROVSSI@users.noreply.github.com>
2024-02-21 12:14:20 +0100

    feat: add dto functionality

---
+++ src/main/java/uca/ac/elearning/ws/dto/disponibiliteProfDetailsDto.java
@@ -0,0 +1,55 @@
+package uca.ac.elearning.ws.dto;
+
+import jakarta.persistence.ManyToOne;
+import uca.ac.elearning.bean.DisponibiliteProf;
+
+import java.util.Date;
+
+public class disponibiliteProfDetailsDto {
+    private long id;
+    private String jour;
+
+    private Date trancheHoraireDebut;
+    private Date trancheHoraireFin;
+    private DisponibiliteProf disponibiliteProf;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getJour() {
+        return jour;
+    }
+
+    public void setJour(String jour) {
+        this.jour = jour;
+    }
+
+    public Date getTrancheHoraireDebut() {
+        return trancheHoraireDebut;
+    }
+
+    public void setTrancheHoraireDebut(Date trancheHoraireDebut) {
+        this.trancheHoraireDebut = trancheHoraireDebut;
+    }
+
+    public Date getTrancheHoraireFin() {
+        return trancheHoraireFin;
+    }
+
+    public void setTrancheHoraireFin(Date trancheHoraireFin) {
+        this.trancheHoraireFin = trancheHoraireFin;
+    }
+
+    public DisponibiliteProf getDisponibiliteProf() {
+        return disponibiliteProf;
+    }
+
+    public void setDisponibiliteProf(DisponibiliteProf disponibiliteProf) {
+        this.disponibiliteProf = disponibiliteProf;
+    }
+}
