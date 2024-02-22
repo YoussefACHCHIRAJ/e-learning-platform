6ad3c6a1180c11e99761008f9a16a338948cf40f
OUZINE AYOUB <ouzineayoub88@gmail.com>
2024-02-22 22:02:06 +0100

    [update] GroupeEtudiantDetailDao

--- src/main/java/uca/ac/elearning/dao/GroupeEtudiantDetailDao.java
+++ src/main/java/uca/ac/elearning/dao/GroupeEtudiantDetailDao.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface GroupeEtudiantDetailDao extends JpaRepository<GroupeEtudiantDetail,Long> {
-
+     List<GroupeEtudiantDetail> findByIsActive(boolean isActive);
 }
