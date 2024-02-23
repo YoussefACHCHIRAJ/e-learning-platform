d9353b5763675614e0f1cb6d5f0153e94adfd3c1
Abderrahmanghazouani <152079577+Abderrahmanghazouani@users.noreply.github.com>
2024-02-23 11:29:24 +0100

    [feat] add description jira

--- src/main/java/uca/ac/elearning/dao/SectionDao.java
+++ src/main/java/uca/ac/elearning/dao/SectionDao.java
@@ -1,14 +1,23 @@
 package uca.ac.elearning.dao;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uca.ac.elearning.bean.Category;
 import uca.ac.elearning.bean.Section;
 
+import java.util.List;
+
 @Repository
 public interface SectionDao extends JpaRepository<Section,Long> {
     Section findByLibelle(String libelle);
+    List<Section> findByCours_Id(Long id);
+
+    int deleteByCours_Id(Long libelle);
+
     int deleteByLibelle(String libelle);
+
+
 
 
 
