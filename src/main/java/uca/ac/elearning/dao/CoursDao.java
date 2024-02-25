e12850455c22d8c7a7a76afa7a920222acb893a1
OUZINE AYOUB <ouzineayoub88@gmail.com>
2024-02-25 20:38:54 +0100

    [Refactor] changed some code in coursDto and GroupeEtudiantDetail

--- src/main/java/uca/ac/elearning/dao/CoursDao.java
+++ src/main/java/uca/ac/elearning/dao/CoursDao.java
@@ -3,11 +3,13 @@ package uca.ac.elearning.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uca.ac.elearning.bean.Cours;
-import uca.ac.elearning.bean.Section;
 
-import java.util.List;
+import java.util.*;
 @Repository
 public interface CoursDao extends JpaRepository<Cours,Long> {
     Cours findByLibelle(String libelle);
     int deleteByLibelle(String libelle);
+
+    List<Cours> findByParcoursId(Long id);
+    int deleteByParcoursId(Long id);
 }
