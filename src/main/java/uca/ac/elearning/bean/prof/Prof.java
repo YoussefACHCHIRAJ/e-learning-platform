648df4e690f14d5b177b4b18f478a5d89f2a472a
ROUSSI Hatim <115400374+ROVSSI@users.noreply.github.com>
2024-02-27 14:17:44 +0100

    [Refactor] reorganize project's files

---
+++ src/main/java/uca/ac/elearning/bean/prof/Prof.java
@@ -0,0 +1,19 @@
+package uca.ac.elearning.bean.prof;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Prof {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String fullName;
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ src/main/java/uca/ac/elearning/webService/dto/studentDto/StudentGroupDetailDto.java
@@ -0,0 +1,9 @@
+package uca.ac.elearning.webService.dto.studentDto;
+
+import lombok.Data;
+
+@Data
+public class StudentGroupDetailDto {
+    private long id;
+    private boolean active;
+}
