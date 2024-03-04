ca3bc7ae5671c2204051b2befeb0f894b47f3fab
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2024-03-04 23:46:05 +0100

    [feat] focus current page
    Refactor the side bar component to highlight the button corresponding
    to the current page.

--- client/src/app/components/side-bar/side-bar.component.ts
+++ client/src/app/components/side-bar/side-bar.component.ts
@@ -1,61 +1,89 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 interface NavigationButtons {
   icon: string,
   content: string,
-  path: string
+  path: string,
+  isActive: boolean
 }
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit{
+
+  private _currentPath!: string;
+
+  public get currentPath(): string {
+    return this._currentPath;
+  }
+  public set currentPath(value: string) {
+    this._currentPath = value;
+  }
 
   private _navigationButtons: Array<NavigationButtons> = [
     {
       icon: "fa-solid fa-house",
       content: "Learn",
-      path: "dashboard/learn"
+      path: "dashboard",
+      isActive: true
     },
     {
       icon: "fas fa-user",
       content: "Profile",
-      path: "dashboard/profile"
+      path: "dashboard/profile",
+      isActive:  false
 
     },
     {
       icon: "fa-solid fa-users-rectangle",
       content: "Groups",
-      path: "dashboard/groups"
+      path: "dashboard/groups",
+      isActive: false
 
     },
     {
       icon: "fa-solid fa-user-tie",
       content: "Profs",
-      path: "dashboard/profs"
+      path: "dashboard/profs",
+      isActive: false
 
     },
     {
       icon: "fa-solid fa-user-group",
       content: "Students",
-      path: "dashboard/students"
+      path: "dashboard/students",
+      isActive: false
 
     },
     {
       icon: "fa-solid fa-calendar-days",
       content: "Calender",
-      path: "dashboard/calendar"
+      path: "dashboard/calendar",
+      isActive: false
 
     },
   ];
 
-
   constructor (private router:Router){}
+  
+  ngOnInit(): void {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+      this.currentPath = this.getCurrentRoute();
+      this.navigationButtons = this.navigationButtons.map(navigateButton => ({
+        ...navigateButton,
+        isActive: `/${navigateButton?.path}` === this.currentPath
+      }));
+      
+    });
+  }
 
   public get navigationButtons(): Array<NavigationButtons> {
     return this._navigationButtons;
@@ -66,6 +94,10 @@ export class SideBarComponent {
 
   navigateTo(path: string){
     this.router.navigate([path]);
+  }
+
+  getCurrentRoute(){
+    return this.router.url;
   }
 
 }
