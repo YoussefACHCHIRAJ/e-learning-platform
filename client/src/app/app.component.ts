64adf1665a1d699e9a3bf2ff924332e7e241646e
yassirab02 <yassirabourial86@gmail.com>
2024-03-04 17:28:18 +0100

    [feat] Add a profs-info component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import {ProfsComponent} from "./pages/profs/profs.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    imports: [RouterOutlet, LoginComponent, SideBarComponent, DashboardComponent]
+  imports: [RouterOutlet, LoginComponent, SideBarComponent, DashboardComponent, ProfsComponent]
 })
 
 export class AppComponent {
---
+++ client/src/app/pages/profs/profs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profs',
+  standalone: true,
+  imports: [],
+  templateUrl: './profs.component.html',
+  styleUrl: './profs.component.css'
+})
+export class ProfsComponent {
+
+}
