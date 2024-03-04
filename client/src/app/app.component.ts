81ac4568d2184d49004fb41d0cc5fd5bbcf8ed7a
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2024-03-04 11:33:25 +0100

    [feat] configure router

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/auth/login/login.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    imports: [RouterOutlet, LoginComponent, SideBarComponent]
+    imports: [RouterOutlet, LoginComponent, SideBarComponent, DashboardComponent]
 })
 
 export class AppComponent {
