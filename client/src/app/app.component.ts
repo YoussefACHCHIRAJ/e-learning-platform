1fcd2d86bee3900ba34e82dcb773384dd346f420
OUZINE AYOUB <ouzineayoub88@gmail.com>
2024-03-07 15:07:07 +0100

    [Refactor] modify profile page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SideBarComponent} from "./components/side-bar/side-bar.component";
+import {CalendarComponent} from "./components/calendar/calendar.component";
+import {UserProfileComponent} from "./components/user-profile/user-profile.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    imports: [RouterOutlet]
+  imports: [RouterOutlet, SideBarComponent, CalendarComponent, UserProfileComponent]
 })
 
 export class AppComponent {
