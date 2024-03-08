b1d3fceae190a1ccd25ef55fa9a9994d3a271e8b
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2024-03-08 10:09:32 +0100

    [feat] Highlight associated navigation button with current page
    Updated Angular code in the SideBarComponent to correctly handle highlighting of the navigation button associated with the current page.
    This includes considering both the current and previous routes, ensuring accurate button highlighting.

--- client/src/app/components/side-bar/side-bar.component.ts
+++ client/src/app/components/side-bar/side-bar.component.ts
@@ -1,18 +1,15 @@
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
-=======
-import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
->>>>>>> 9fe287b2bff0a3f2e9b003f5f05d1083ca6b8459
+
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { navigationButtonsValues } from '../../../constants';
 
 interface NavigationButtons {
-  icon: string,
-  content: string,
-  path: string,
-  isActive: boolean
+  icon: string;
+  content: string;
+  path: string;
+  isActive: boolean;
 }
 
 @Component({
@@ -21,78 +18,19 @@ interface NavigationButtons {
   imports: [CommonModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
-export class SideBarComponent implements OnInit{
+export class SideBarComponent implements OnInit {
+  private _navigationButtons!: Array<NavigationButtons>;
 
-  private _currentPath!: string;
+  constructor(private router: Router) {}
 
-  public get currentPath(): string {
-    return this._currentPath;
-  }
-  public set currentPath(value: string) {
-    this._currentPath = value;
-  }
-
-   private _navigationButtons: Array<NavigationButtons> = [
-    {
-      icon: "fa-solid fa-house",
-      content: "Learn",
-<<<<<<< HEAD
-      path: "dashboard",
-      isActive: true
-=======
-      path: "dashboard"
->>>>>>> 9fe287b2bff0a3f2e9b003f5f05d1083ca6b8459
-    },
-    {
-      icon: "fas fa-user",
-      content: "Profile",
-      path: "dashboard/profile",
-      isActive:  false
-
-    },
-    {
-      icon: "fa-solid fa-users-rectangle",
-      content: "Groups",
-      path: "dashboard/groups",
-      isActive: false
-
-    },
-    {
-      icon: "fa-solid fa-user-tie",
-      content: "Profs",
-      path: "dashboard/profs",
-      isActive: false
-
-    },
-    {
-      icon: "fa-solid fa-user-group",
-      content: "Students",
-      path: "dashboard/students",
-      isActive: false
-
-    },
-    {
-      icon: "fa-solid fa-calendar-days",
-      content: "Calender",
-      path: "dashboard/calendar",
-      isActive: false
-
-    },
-  ];
-
-  constructor (private router:Router){}
-  
   ngOnInit(): void {
-    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
-      this.currentPath = this.getCurrentRoute();
-      this.navigationButtons = this.navigationButtons.map(navigateButton => ({
-        ...navigateButton,
-        isActive: `/${navigateButton?.path}` === this.currentPath
-      }));
-      
-    });
+    this.navigationButtons = navigationButtonsValues;
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => this?.updateNavigationButton());
   }
 
   public get navigationButtons(): Array<NavigationButtons> {
@@ -102,12 +40,15 @@ export class SideBarComponent implements OnInit{
     this._navigationButtons = value;
   }
 
-  navigateTo(path: string){
+  navigateTo(path: string) {
     this.router.navigate([path]);
   }
 
-  getCurrentRoute(){
+  getCurrentRoute() {
     return this.router.url;
   }
 
+  updateNavigationButton() {
+    this.navigationButtons.forEach(navigateButton => navigateButton.isActive = `/${navigateButton?.path}` === this.getCurrentRoute());
+  }
 }
