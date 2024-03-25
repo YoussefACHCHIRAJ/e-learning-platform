0dd05b2839a5340206391abb8af76806bb7d13bd
Nouma Houssameddine <houssameddinenouma@gmail.com>
2024-03-25 23:19:37 +0000

    [Refactor] Initializing prof-extra-registration component

---
+++ client/src/app/components/prof-extra-registration/prof-extra-registration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prof-extra-registration',
+  standalone: true,
+  imports: [],
+  templateUrl: './prof-extra-registration.component.html',
+  styleUrl: './prof-extra-registration.component.css'
+})
+export class ProfExtraRegistrationComponent {
+
+}
