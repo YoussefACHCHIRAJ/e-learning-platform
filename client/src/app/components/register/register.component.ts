37eeb2791ebc2e6db31f8f3abba347c0588f5892
Abderrahmanghazouani <152079577+Abderrahmanghazouani@users.noreply.github.com>
2024-03-27 13:37:06 +0000

    finish paths

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
