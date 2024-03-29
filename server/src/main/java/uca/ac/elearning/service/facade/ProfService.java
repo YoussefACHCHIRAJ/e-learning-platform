77bdcfc4ccb86caecd4f32a3fe02cf94a146fadd
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2024-03-29 12:28:25 +0000

    [fix] fixing idiot conflicts

--- server/src/main/java/uca/ac/elearning/service/facade/ProfService.java
+++ server/src/main/java/uca/ac/elearning/service/facade/ProfService.java
@@ -1,9 +1,4 @@
 package uca.ac.elearning.service.facade;
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 728a08b8d7736ae2f92c53dc3130f1b8a610e60a
 import org.springframework.web.bind.annotation.RequestBody;
 import uca.ac.elearning.bean.common.Day;
 import uca.ac.elearning.utils.ProfRequest;
@@ -15,11 +10,5 @@ public interface ProfService {
     List<Day> findAllDays();
 
     ProfResponse saveAvailability(@RequestBody ProfRequest request);
-<<<<<<< HEAD
 
-=======
-=======
-public interface ProfService {
->>>>>>> 8c697f47cde7ed4da35e778026c20b72bcaa1972
->>>>>>> 728a08b8d7736ae2f92c53dc3130f1b8a610e60a
 }
