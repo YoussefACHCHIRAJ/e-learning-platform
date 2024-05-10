33e981f0521a623c3d67c1fd95a209867550ed1b
Nouma Houssameddine <houssameddinenouma@gmail.com>
2024-05-10 10:38:30 +0100

    [feat] Implementing User web service by creatingendpoitns for getting both students and profs

--- server/src/main/java/uca/ac/elearning/dao/users/UserDao.java
+++ server/src/main/java/uca/ac/elearning/dao/users/UserDao.java
@@ -2,12 +2,16 @@ package uca.ac.elearning.dao.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import uca.ac.elearning.bean.users.Role;
 import uca.ac.elearning.bean.users.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+    List<User> findByRole(Role role);
 }
