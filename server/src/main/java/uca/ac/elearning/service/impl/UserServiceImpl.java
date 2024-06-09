7e993d6232758653698859848805163f5e270147
Nouma Houssameddine <houssameddinenouma@gmail.com>
2024-06-09 13:16:46 +0100

    refactor : tiny changes

--- server/src/main/java/uca/ac/elearning/service/impl/UserServiceImpl.java
+++ server/src/main/java/uca/ac/elearning/service/impl/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package uca.ac.elearning.service.impl;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import uca.ac.elearning.bean.users.Role;
 import uca.ac.elearning.bean.users.User;
 import uca.ac.elearning.dao.users.UserDao;
 import uca.ac.elearning.service.facade.UserService;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
 
